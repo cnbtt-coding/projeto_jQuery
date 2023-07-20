3fd528e14feda5e162943f5cb2191deeabcfbc9b
Renato Nascimento Battista <Renatinho2086@gmail.com>
2023-07-20 18:58:04 -0300

    Feat: added slide down and up on 'add-pic' bar

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-    console.log($('#newPicButton'))
+
+    $('#newPicButton').click(function() {
+        $('.add-pic').slideDown();
+    }) // Selecionando o botão de adicionar foto e implementando comportamento de abrir aba ao clicar
+
+    $('add').click(function() {
+        
+    }) // Selecionando o botão de adicionar e implementando função de adicionar foto no conteúdo principal da página
+
+    $('.cancel').click(function() {
+        $('.add-pic').slideUp();
+    }) // Selecionando o botão de cancelar ao abrir a aba de adicionar foto e implementando comportamendo de fechar aba ao clicar
+
 })
